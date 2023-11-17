82cc539cbcc2d8192ff636b8cbe75df1de00159e
samade747 <samad.e747@gmail.com>
2023-11-18 00:25:17 +0500

    1224

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const btns = document.querySelectorAll('.btn');
+// slide no 208 - 209
+// const btns = document.querySelectorAll('.btn');
 
-btns.forEach(function(btn){
-    btn.addEventListener('click', function(e) {
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e) {
+//         console.log(e.currentTarget);
+//         e.currentTarget.style.color = 'green';
+//     });
+// });
+
+
+
+// slide no 210 
+
+const btn1 = document.querySelectorAll('.btn');
+
+btn1.forEach(function(btn){
+    btn.addEventListener('click', function(e){
         console.log(e.currentTarget);
-        e.currentTarget.style.color = 'green';
-    });
-});
+        e.currentTarget.style.color = 'red'
+    })
+})
